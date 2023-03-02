88742abfa024bb42e4b8e8633bc43bc3989fde7e
bsnacks000 <bsnacks000@gmail.com>
2023-03-02 08:35:35 -0500

    add basic mixins

--- tests/test_dal_basics.py
+++ tests/test_dal_basics.py
@@ -1,28 +1,35 @@
 import pytest
-import sqlalchemy as sa 
-import datetime 
+import sqlalchemy as sa
+import datetime
 
 pytestmark = pytest.mark.anyio
 
 
+async def test_dal_basics(transaction):
+    author = transaction.get_table("author")
+    book = transaction.get_table("book")
 
-async def test_dal_basics(transaction):  
+    assert transaction.get_unique_constraint("book") == ["catalog"]
 
-    author = transaction.get_table('author')
-    book = transaction.get_table('book')
-
-    assert transaction.get_unique_constraint('book') == ['catalog']
-
-    stmt = sa.insert(author).values(name='bsnacks').returning(author.c.id, author.c.name)
+    stmt = (
+        sa.insert(author).values(name="bsnacks").returning(author.c.id, author.c.name)
+    )
     result = await transaction.execute(stmt)
 
     me = result.one()
-    stmt = sa.insert(book).values(
-        author_id=me.id, 
-        name='some book', 
-        catalog='123abc', 
-        extra={'some': 'value', 'date': datetime.datetime.now().date()})
 
-    _ = await transaction.execute(stmt)
+    assert me.name == "bsnacks"
+    stmt = (
+        sa.insert(book)
+        .values(
+            author_id=me.id,
+            name="some book",
+            catalog="123abc",
+            extra={"some": "value", "date": datetime.datetime.now().date()},
+        )
+        .returning(book.c.id, book.c.name)
+    )
 
-    
+    result = await transaction.execute(stmt)
+    mybook = result.one()
+    assert mybook.name == "some book"

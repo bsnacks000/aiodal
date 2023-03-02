70398fff15606959b902649dd3536603ea5a32b9
bsnacks000 <bsnacks000@gmail.com>
2023-03-02 09:05:06 -0500

    add smoke test for versioning

---
+++ tests/test_version.py
@@ -0,0 +1,14 @@
+import pytest
+import pathlib
+import aiodal
+import toml
+
+
+def test_pyproject_version_sync():
+    path = pathlib.Path(__file__).resolve().parents[1] / "pyproject.toml"
+    pyproject = toml.loads(open(str(path)).read())
+    pyproject_version = pyproject["tool"]["poetry"]["version"]
+
+    package_init_version = aiodal.__version__
+
+    assert package_init_version == pyproject_version

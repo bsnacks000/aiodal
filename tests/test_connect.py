b8ef74521c47684e73de6d157e41bc9be7fc7501
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2023-05-16 12:35:14 -0400

    Temporary mocker commit

---
+++ tests/test_connect.py
@@ -0,0 +1,13 @@
+import pytest
+import sqlalchemy as sa
+
+from aiodal import connect
+
+pytestmark = pytest.mark.anyio
+
+
+async def test_connect_module(mocker):
+    print(mocker)
+
+    kwargs = {"url": "http://www.google.com/"}
+    db = await connect.or_fail(**kwargs)

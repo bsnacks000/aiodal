08d324136c7628a646a7a02d522d9aa021b23fc5
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2023-05-17 16:48:36 -0400

    Clean up getter setter test

--- tests/test_dal.py
+++ tests/test_dal.py
@@ -38,20 +38,27 @@ async def test_dal_basics(transaction):
     await transaction.rollback()
 
 
-async def test_dal_setters_getters(engine_uri):
+async def test_dal_setters_getters(engine_uri, transaction):
+    db = await connect.or_fail(url=engine_uri, with_exit=False)
     tablename = "Table name"
     value = "Testing value"
-    obj = dal.DataAccessLayer()
-    obj.set_aliased(name=tablename, t=value)
-    assert obj._aliased_tables[tablename] == value
-    assert obj.get_aliased(name=tablename) == value
 
-    db = await connect.or_fail(url=engine_uri, with_exit=False)
+    db.set_aliased(name=tablename, t=value)
+    assert db._aliased_tables[tablename] == value
+    assert db.get_aliased(name=tablename) == value
+
+    transaction.set_aliased(name=tablename, t=value)
+    assert transaction._aliased_tables[tablename] == value
+    assert transaction.get_aliased(name=tablename) == value
+
+    with pytest.raises(KeyError):
+        transaction.get_aliased(name="nonexistent table")
+
     author_table = db.get_table("author")
     assert author_table is not None
 
     with pytest.raises(KeyError):
-        this_table_doesnt_exist = db.get_table("nonexistent table")
+        db.get_table("nonexistent table")
 
     book_constraint = db.get_unique_constraint("book")
     assert book_constraint == ["catalog"]

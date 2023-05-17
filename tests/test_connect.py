b8728b4e2cc23760e2ead93fe9070e75fe5cf251
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2023-05-17 01:31:12 -0400

    Implement connect test

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -1,5 +1,5 @@
 import pytest
-import sqlalchemy as sa
+from sqlalchemy.exc import SQLAlchemyError
 
 from aiodal import connect
 
@@ -7,7 +7,29 @@ pytestmark = pytest.mark.anyio
 
 
 async def test_connect_module(mocker):
-    print(mocker)
+    mocked = mocker.patch.object(connect, "connect")
 
-    # kwargs = {"url": "http://www.google.com/"}
-    # db = await connect.or_fail(**kwargs)
+    # Basic test
+    await connect.or_fail(url="abc123")
+    assert await mocked.called_once()
+    assert await mocked.called_with(url="abc123")
+
+    mocked.reset_mock()
+
+    # Ensure engine options are propagated correctly
+    await connect.or_fail(url="abc123", engine_option_1="test")
+    assert await mocked.called_once()
+    assert await mocked.called_with(url="abc123", engine_option_1="test")
+
+    mocked.reset_mock()
+
+    # Check that SQLAlchemyErrors are propagated correctly
+    mocked.side_effect = SQLAlchemyError
+    with pytest.raises(SQLAlchemyError):
+        await connect.or_fail(url="abc123", with_exit=False)
+    assert await mocked.called_once()
+
+    # Check that SQLAlchemyErrors generate a sys.exit if with_exit is not False
+    mocked.side_effect = SQLAlchemyError
+    with pytest.raises(SystemExit):
+        await connect.or_fail(url="abc123")

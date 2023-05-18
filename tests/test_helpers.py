19cc5cdd5fafd3a68ba110a56faa2528a73b746b
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2023-05-17 21:43:08 -0400

    Add helper test stubs

---
+++ tests/test_helpers.py
@@ -0,0 +1,14 @@
+import pytest
+import sqlalchemy
+
+from aiodal import helpers
+
+pytestmark = pytest.mark.anyio
+
+
+async def test_json_serialize():
+    ...
+
+
+async def test_total_count(transaction):
+    ...

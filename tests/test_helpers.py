5158826b56254e82453a5910e127250c77e57914
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2023-05-17 23:25:09 -0400

    Add test to cover total count

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,5 @@
 import pytest
-import sqlalchemy
+import sqlalchemy as sa
 
 from aiodal import helpers
 
@@ -11,4 +11,12 @@ async def test_json_serialize():
 
 
 async def test_total_count(transaction):
-    ...
+    author = transaction.get_table("author")
+    stmt = sa.insert(author).values(name="kgas").returning(author.c.id, author.c.name)
+    result = await transaction.execute(stmt)
+    assert result.one().name == "kgas"
+
+    stmt = sa.select(author.c.id, author.c.name, helpers.sa_total_count(author.c.id))
+    result = await transaction.execute(stmt)
+    result = result.one()
+    assert result.total_count == 1

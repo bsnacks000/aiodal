9bff93ee2e2a5c1be6b5c9147d39cec6332626ea
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2024-06-17 13:20:02 -0400

    Add script to fetch access token

---
+++ scripts/get_access_token.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+set -e
+
+client_secret=$CLIENT_SECRET
+
+response=$(curl -s --request POST \
+  --url https://dev-qfnm6uuqxtjs3l44.us.auth0.com/oauth/token \
+  --header 'content-type: application/json' \
+  --data "{\"client_id\":\"DsfPLEIFU5Gn9qmhTuMnqiV8Irs3fUi3\",\"client_secret\":\"$client_secret\",\"audience\":\"https://testing.api\",\"grant_type\":\"client_credentials\"}")
+
+access_token=$(echo $response | jq -r '.access_token')
+
+echo $access_token
